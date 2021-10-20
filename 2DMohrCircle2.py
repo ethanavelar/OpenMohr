24af10dc3ee18ce305420c50eed44498d8654d04
Ethan Taylor <taylorjethan@gmail.com>
2021-10-20 17:56:59 -0500

    Stress Element Diagram Rotation
    Introduced a point rotation function to rotate stress element points for the diagram. Axis reference line how rotates and translates to look more natural with the rotation of the element. Second stress arrow was added and a label was added to the stress element for shear stress. Stress element is how callable and is able to take an angle of rotation in degrees.

--- 2DMohrCircle2.py
+++ 2DMohrCircle2.py
@@ -2,6 +2,81 @@ import numpy as np
 import matplotlib.pyplot as pl
 import matplotlib.patches as patches
 
+class element:
+    def __init__(self):
+        self.edgePointsX =[0.25, 3.75, 4.25, 4.25, 3.75, 0.25, -0.25, -0.25]
+        self.edgePointsY = [-0.25, -0.25, 0.25, 3.75, 4.25, 4.25, 3.75, 0.25]
+        self.midPointsX = [-0.25, 2, 2, 4.25]
+        self.midPointsY = [2, -0.25, 4.25, 2]
+        self.midOffPointsX = [-2, 2, 2, 6]
+        self.midOffPointsY = [2, -2, 6, 2]
+        self.allRotateablePointsX = [0.25, 3.75, 4.25, 4.25, 3.75, 0.25, -0.25, -0.25, -0.25, 2, 2, 4.25, -2, 2, 2, 6]
+        self.allRotateablePointsY = [-0.25, -0.25, 0.25, 3.75, 4.25, 4.25, 3.75, 0.25, 2, -0.25, 4.25, 2, 2, -2, 6, 2]
+    @classmethod
+    def rotate(self,x,y,angle): #rotate x,y around xo,yo by theta (rad)
+        theta = np.deg2rad(angle)
+        xp, yp = [], []
+        for k in range(len(x)):
+            xr=np.cos(theta)*(x[k])-np.sin(theta)*(y[k])
+            yr=np.sin(theta)*(x[k])+np.cos(theta)*(y[k])
+            xp.append(xr)
+            yp.append(yr)
+        return xp, yp
+    def buildElement(self, angle):
+        fig = pl.figure(tight_layout=True)
+        ax = fig.add_subplot(1,1,1)
+        # Determine Edge Points of Square/Element
+        #x = [-0.25, 4.25, 4.25, -0.25]
+        #y = [-0.25, -0.25, 4.25, 4.25]
+        x,y = self.rotate(self.edgePointsX, self.edgePointsY, angle)
+        
+        #ax.scatter(x,y)
+        # Midpoints of the Square/Element
+        x1, y1 = self.midPointsX, self.midPointsY#self.rotate(self.midPointsX, self.midOffPointsY, angle)
+        #ax.scatter(x1, y1)
+        # Midpoints that are off of
+        x1, x2 = [], []
+        if (angle == 0):
+            x1, y1 = self.midPointsX, self.midPointsY
+            x2, y2 = self.midOffPointsX, self.midOffPointsY
+        else:
+            x1, y1 = self.rotate(self.midPointsX, self.midPointsY, angle)
+            x2, y2 = self.rotate(self.midOffPointsX, self.midOffPointsY, angle)
+        #ax.scatter(x2, y2)
+        ax.add_patch(patches.Rectangle((0,0), 4,4, ec='k', fc=None, color=None, angle=angle))
+        centerMovementX, CenterMovementY = self.rotate([2], [2], angle)
+        ax.plot([centerMovementX, [centerMovementX[0]+5]], [CenterMovementY, CenterMovementY], color='k', zorder=0 )
+
+        #ax.arrow(0, -0.25, 4, 0, length_includes_head=True, head_width=0.2)
+        # Arrow to Point Arrangement for Shear Stress Arrows
+        ArrowArr = [0, 2, 4, 6]
+        #print(ArrowArr)
+        # List Handles are Intended to Provide Flexability with Arrows
+        # Looking to figure out how to flip the direction of two arrows
+        xlistHandle = x
+        ylistHandle = y
+        for i in ArrowArr:
+            dx = x[i+1] - x[i]
+            dy = y[i+1] - y[i]
+            ax.arrow(x[i], y[i], dx, dy, length_includes_head=False, head_width=0.2, shape='left', color='k')
+            print(x[i], y[i], dx, dy)
+        
+        for e in range(len(x1)):
+            dx = x2[e] - x1[e]
+            dy = y2[e] - y1[e]
+            ax.arrow(x1[e], y1[e], dx, dy, length_includes_head=True, head_width=0.2, color='k')
+        
+        ax.annotate(
+                r'$\tau_{xy}$',
+                (x[4], y[4]),
+                textcoords='offset points',
+                xytext= (10,15),
+                ha = 'left'
+            )
+
+        ax.set_aspect('equal')
+        pl.show()
+
 def stress(sigmax, sigmay, txy):
     # Principle Stresses
 
@@ -117,64 +192,26 @@ def buildMohrCircle(sigmax, sigmay, txy, dire, sigma1, sigma2):
     ax.axvline(x=0, color='k')
     ax.set_aspect('equal')
     pl.show()
-
-def buildPrincipleStressElement():
+'''
+def buildMaxShearElement(x, y, t1, phis):
+    x, y = rotate(x, y, 0, 0)
+    print(xp)
     fig = pl.figure(tight_layout=True)
     ax = fig.add_subplot(1,1,1)
-    # Determine Edge Points of Square/Element
-    #x = [-0.25, 4.25, 4.25, -0.25]
-    #y = [-0.25, -0.25, 4.25, 4.25]
-
-    x = [0.25, 3.75, 4.25, 4.25, 3.75, 0.25, -0.25, -0.25]
-    y = [-0.25, -0.25, 0.25, 3.75, 4.25, 4.25, 3.75, 0.25]
-
-    ax.scatter(x,y)
-    # Midpoints of the Square/Element
-    x1 = [-0.25, 2, 2, 4.25]
-    y1 = [2, -0.25, 4.25, 2]
-    ax.scatter(x1, y1)
-    # Midpoints that are off of
-    x2 = [-2, 2, 2, 6]
-    y2 = [2, -2, 6, 2]
-    ax.scatter(x2, y2)
-    
-    phip = -30
-    ax.add_patch(patches.Rectangle((0,0), 4,4)) #, angle=phip))    
-    ax.plot([2, 7], [2, 2])
-
-    #ax.arrow(0, -0.25, 4, 0, length_includes_head=True, head_width=0.2)
-    # Arrow to Point Arrangement for Shear Stress Arrows
-    ArrowArr = [0, 2, 4, 6]
-    print(ArrowArr)
-    # List Handles are Intended to Provide Flexability with Arrows
-    # Looking to figure out how to flip the direction of two arrows
-    xlistHandle = x
-    ylistHandle = y
-    for i in ArrowArr:
-        dx = x[i+1] - x[i]
-        dy = y[i+1] - y[i]
-        ax.arrow(x[i], y[i], dx, dy, length_includes_head=False, head_width=0.2, shape='left', color='k', head_starts_at_zero=False)
-        print(x[i], y[i], dx, dy)
-    
-    for e in range(len(x1)-1):
-        dx = x2[e] - x1[e]
-        dy = y2[e] - y1[e]
-        ax.arrow(x1[e], y1[e], dx, dy, length_includes_head=True, head_width=0.2)
-
-    ax.set_aspect('equal')
+    ax.scatter(xp, yp)
     pl.show()
-
-def buildMaxShearElement():
-    pass
-
+'''
 def main():
+    maxelement = element()
     sigmax = 20
     sigmay = -10
     txy = 10
     direction = "cw"
 
     sigma1, sigma2 = stress(sigmax, sigmay, txy)
-    buildMohrCircle(sigmax, sigmay, txy, direction, sigma1, sigma2)
-    buildPrincipleStressElement()
+    #buildMohrCircle(sigmax, sigmay, txy, direction, sigma1, sigma2)
+    maxelement.buildElement(10)
+    #buildPrincipleStressElement()
+    #buildMaxShearElement(x,y, 10, 20)
     
 if __name__ == "__main__": main()
